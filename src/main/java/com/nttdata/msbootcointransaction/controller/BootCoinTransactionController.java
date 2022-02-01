9869268e3cb3ce02087095b257d412b4c1c4cbcc
Roram <68767119+Roram-io@users.noreply.github.com>
2022-02-01 16:04:27 -0500

    Update BootCoinTransactionController.java

--- src/main/java/com/nttdata/msbootcointransaction/controller/BootCoinTransactionController.java
+++ src/main/java/com/nttdata/msbootcointransaction/controller/BootCoinTransactionController.java
@@ -1,4 +1,31 @@
 package com.nttdata.msbootcointransaction.controller;
 
+import com.nttdata.msbootcointransaction.model.BootCoinTransaction;
+import com.nttdata.msbootcointransaction.service.BootCoinTransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/bootcointransaction")
 public class BootCoinTransactionController {
+
+    @Autowired
+    BootCoinTransactionService bootCoinTransactionService;
+
+    @PostMapping("/create")
+    public Mono<BootCoinTransaction> createBootCoinTransaction(@RequestBody BootCoinTransaction bootCoinTransaction){
+        return bootCoinTransactionService.createBootCoinTransaction(bootCoinTransaction);
+    }
+
+    @PostMapping("/transaction")
+    public Mono<BootCoinTransaction> processBootCoinTransaction(@RequestParam("id") String id, @RequestParam("response") String response){
+        return bootCoinTransactionService.processBootCoinTransaction(id, response);
+    }
+
+    @GetMapping("/listAll")
+    public Flux<BootCoinTransaction> getTransactions(){
+        return bootCoinTransactionService.getTransactions();
+    }
 }
